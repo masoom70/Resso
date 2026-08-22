e9f02951a7d11b03de364b413a5c3aa745412b2b
AbhiModsz <134328176+AbhiModsz@users.noreply.github.com>
2026-08-22 19:17:07 +0530

    Update __main__.py
    Signed-off-by: AbhiModsz <134328176+AbhiModsz@users.noreply.github.com>

--- anony/__main__.py
+++ anony/__main__.py
@@ -1,31 +1,18 @@
-# Copyright (c) 2025 AnonymousX1025
-# Licensed under the MIT License.
-# This file is part of AnonXMusic
-
-
 import asyncio
 import signal
 import importlib
+from contextlib import suppress
 
-from anony import (anon, app, config, db, logger,
-                   stop, thumb, userbot, yt)
+from anony import (anon, app, config, db,
+                   logger, stop, userbot, yt)
 from anony.plugins import all_modules
-
 
 async def idle():
     loop = asyncio.get_running_loop()
     stop_event = asyncio.Event()
-
-    try:
-        for sig in (signal.SIGINT, signal.SIGTERM, signal.SIGABRT):
+    for sig in (signal.SIGINT, signal.SIGTERM, signal.SIGABRT):
+        with suppress(NotImplementedError):
             loop.add_signal_handler(sig, stop_event.set)
-    except NotImplementedError:
-        def handler(sig, frame):
-            loop.call_soon_threadsafe(stop_event.set)
-
-        signal.signal(signal.SIGINT, handler)
-        signal.signal(signal.SIGTERM, handler)
-
     await stop_event.wait()
 
 async def main():
@@ -33,7 +20,6 @@ async def main():
     await app.boot()
     await userbot.boot()
     await anon.boot()
-    await thumb.start()
 
     for module in all_modules:
         importlib.import_module(f"anony.plugins.{module}")
@@ -45,20 +31,22 @@ async def main():
     sudoers = await db.get_sudoers()
     app.sudoers.update(sudoers)
     app.bl_users.update(await db.get_blacklisted())
-    logger.info(f"Loaded {len(app.sudoers)} sudo users.")
-
-    await yt.fallen.get_session()
+    
+    from NovaTuneMusic.helpers import thumb
+    if config.API_KEY: await yt.fallen.get_session()
+    await thumb.get_session()
 
     await idle()
     await stop()
-    await yt.fallen.session.close()
+    
+    await yt.fallen.close()
     await thumb.session.close()
-
 
 if __name__ == "__main__":
     try:
-        asyncio.run(main())
+        asyncio.get_event_loop().run_until_complete(main())
     except KeyboardInterrupt:
         pass
-    except Exception as ex:
-        raise SystemExit(ex)
+    except Exception:
+        logger.exception("Main Loop Error:")
+      
